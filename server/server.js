e2c77b3bcc56ad57b889b9a8d2ec6146c3997f13
Prithivraj200 <prethive200@gmail.com>
2018-02-22 21:57:24 +0530

    v1.1.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
  const path=require('path');
  const express=require('express');
+ const http=require('http');
+ const socketIO=require('socket.io');
 
  const templatePath=path.join(__dirname,'../client');
- var app=express();
  var port=process.env.PORT || 3000;
+ var app=express();
+ var server=http.createServer(app);
+ var io=socketIO(server);
+
+ io.on('connection',(socket)=>{
+ 	console.log('User connected');
+ 	socket.on('disconnect',()=>{
+ 		console.log('User disconnected');
+ 	})
+ })
 
  app.use(express.static(templatePath));
 
- app.listen(port,()=>{
+ server.listen(port,()=>{
  	console.log(`Server running in ${port}`)
  })
